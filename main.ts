38b30b74d6048b90cc882208b69c75ddaa68c162
contatoanajuliafolheados-spec <contato.anajuliafolheados@gmail.com>
2026-05-13 17:54:14 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.showNumber(12)
+    basic.showIcon(IconNames.Heart)
+})
